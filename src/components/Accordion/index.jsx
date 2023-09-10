60014616c1e12f754a97c1ab45067ea964dd3ffd
Yusuf <136828499+hiasy@users.noreply.github.com>
2023-09-10 17:05:08 +0300

    DESIGN ADJUSTMENTS

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -12,24 +12,24 @@ export default function Accordion() {
   }
   const accordionData = [
     {
-      question: 'Do you design illustration website?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: 'What is EYES on AI',
+      answer: 'EYES on AI is a technology solution designed to assist blind and visually impaired individuals. It integrates AI Vision, AI Monitoring, Voice Assistance powered by Google PaLM, and a Product Detector to provide a comprehensive, user-friendly experience.'
     },
     {
-      question: 'Do you provide design source file after finish work?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: 'What are the different service levels you offer?',
+      answer: 'We offer three tiers of service: Standard, Professional, and Ultimate. Each tier provides a different set of features and capabilities designed to meet varied user needs. For more details, please refer to our "Our Service Levels" section.'
     },
     {
-      question: 'How to provide project details and payments?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: 'How do I upgrade my service level?',
+      answer: 'You can upgrade your service level through the EYES on AI application or by contacting our customer support team.'
     },
     {
-      question: 'Can you tell me please how to contact for project?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: 'Is EYES on AI compatible with my device?',
+      answer: 'EYES on AI is designed to be compatible with a wide range of smartphones and smart glasses. For specific device compatibility, please contact our support team.'
     },
     {
-      question: 'Do you makes custom logo, icon etc?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: 'How does the Voice Assistance feature work?',
+      answer: 'Our Voice Assistance uses Google PaLM for natural language understanding. You can ask questions, receive directions, and interact with your environment through simple voice commands.'
     },
   ]
   return (
--- src/components/Widget/SocialWidget.jsx
+++ src/components/Widget/SocialWidget.jsx
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom'
 import { Icon } from '@iconify/react';
 import Div from '../Div';
 
+
+
 export default function SocialWidget() {
   return (
     <Div className="cs-social_btns cs-style1">
-      <Link to='/' className="cs-center">
+      <Link to='https://www.linkedin.com/in/althawadi7/' className="cs-center">
         <Icon icon="fa6-brands:linkedin-in" />
       </Link>
-      <Link to='/' className="cs-center">
+      <Link to='https://twitter.com/i/flow/login?redirect_after_login=%2Fiinjaztoken' className="cs-center">
         <Icon icon="fa6-brands:twitter" />               
       </Link>
-      <Link to='/' className="cs-center">
+      <Link to='https://www.youtube.com/watch?v=n-u5FNchaBs' className="cs-center">
         <Icon icon="fa6-brands:youtube" />              
       </Link>
-      <Link to='/' className="cs-center">
+      <Link to='https://app.slack.com/client/T03JBCX8WE7/C05BR9R2C5P/thread/C05BR9R2C5P-1694348563.348019' className="cs-center">
         <Icon icon="fa6-brands:slack" />
       </Link>
     </Div>
