179d8f0189d80dede7a0f542bd26e606dd43d0b0
althawadi <althawadir7@gmail.com>
2023-09-13 12:51:26 +0300

    fixed the path

--- express-AyesOnAi/routes/users.js
+++ express-AyesOnAi/routes/users.js
@@ -1,14 +1,14 @@
 // file : AyesOnAi\express-AyesOnAi\routes\users.js
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const usersController = require('../controllers/api/users');
+const usersController = require("../controllers/api/users");
 
-router.delete('/:id', usersController.deleteUser);
-router.get('/:id', usersController.getUserById);
-router.put('/update/:id', usersController.updateUserById);
-router.post('/create', usersController.create);
+router.delete("/:id/delete", usersController.deleteUser);
+router.get("/:id", usersController.getUserById);
+router.put("/update/:id", usersController.updateUserById);
+router.post("/create", usersController.create);
 
-router.get('/', usersController.getAllUsers);
+router.get("/", usersController.getAllUsers);
 
 module.exports = router;
